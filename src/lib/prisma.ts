e9c3c179d022740d5f19b65979582c013ad4f9dc
monichre <liamhellis@gmail.com>
2023-04-06 19:12:57 -0400

    added new prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,3 @@
-// import { PrismaClient } from '@prisma/client';
-import { PrismaClient } from '@prisma/client/edge'
-
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-//
-// Learn more:
-// https://pris.ly/d/help/next-js-best-practices
+import { PrismaClient } from '@prisma/client'
 
 export const prisma = new PrismaClient()
